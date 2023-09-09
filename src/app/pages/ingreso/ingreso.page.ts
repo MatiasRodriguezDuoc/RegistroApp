5dba7fa7204c22f7966eeead43031865caa58783
FJosephx <francounda2004@gmail.com>
2023-09-08 23:59:56 -0300

    Login validado, ahora muestra un mensaje en rojo cuando las credenciales son incorrectas y cuando son correctas redirige a inicio.html

--- src/app/pages/ingreso/ingreso.page.ts
+++ src/app/pages/ingreso/ingreso.page.ts
@@ -1,17 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ingreso',
   templateUrl: './ingreso.page.html',
   styleUrls: ['./ingreso.page.scss'],
 })
+
 export class IngresoPage {
   public correo = '';
   public contrasena = '';
 
-  iniciarSesion() {
-    // Aquí puedes agregar la lógica de autenticación
-    // Por ejemplo, comparar correo y contraseña con datos en la base de datos
-    // Si la autenticación es exitosa, puedes navegar a la página de inicio.
+  usuarios = [
+    {
+      correo: 'franco@duocuc.cl',
+      contrasena: '1234',
+      nombre: 'Franco',
+    },
+    {
+      correo: 'francisco@profesor.cl',
+      contrasena: 'duoc123',
+      nombre: 'Francisco',
+    }
+  ];
+
+  constructor (private router: Router, private toastController: ToastController) {}
+
+  async iniciarSesion() {
+    const usuarioEncontrado = this.usuarios.find(
+      (usuario) => usuario.correo === this.correo && 
+      usuario.contrasena === this.contrasena);
+
+    if (usuarioEncontrado) {
+      this.router.navigate (['/inicio'], {
+        state: {nombreUsuario: usuarioEncontrado.nombre},
+      });
+    } else {
+      const toast = await this.toastController.create({
+        message: 'Credenciales de inicio de sesión incorrectas',
+        duration: 2000,
+        position: 'middle',
+        color: 'danger',
+      });
+
+      toast.present();
+    
+    }
   }
 }

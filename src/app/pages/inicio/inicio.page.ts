240aba47de69866969d9474d8d8e25241bd2d72c
FJosephx <francounda2004@gmail.com>
2023-09-09 00:42:03 -0300

    login redirige a inicio correctamente y muestra mensaje de bienvenida con nombre del usuario

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
+
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
-  nombreUsuario = 'Usuario de Ejemplo';
+export class InicioPage {
+  nombreUsuario = '';
 
-  constructor(private router: Router) {}
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  constructor(private router: Router) {
+    const state = this.router.getCurrentNavigation()?.extras.state;
+    if (state && state['nombreUsuario']) {
+      this.nombreUsuario = state['nombreUsuario'];
+    }
+  }
+
+
+  abrirCamara() {
+    // Aquí puedes implementar la lógica para abrir la cámara y leer un QR
+    // Esto puede involucrar el uso de plugins o librerías externas.
   }
 
   subirArchivo() {
